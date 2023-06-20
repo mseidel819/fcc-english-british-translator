fc360768331fd61ada55300dfec97cac6348ea28
Matt Seidel <mseidel819@gmail.com>
2023-06-20 14:04:41 -0500

    initial commit

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,66 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
+
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
 
 class Translator {
+  translateToBrittish(text) {
+    const textArray = text.split(" ");
+    const translatedText = textArray.map((word) => {
+      if (americanOnly[word.toLowerCase()]) {
+        return `<span class="highlight">${americanOnly[word]}</span>`;
+      } else if (americanToBritishSpelling[word.toLowerCase()]) {
+        return `<span class="highlight">${americanToBritishSpelling[word]}</span>`;
+      } else if (americanToBritishTitles[word.toLowerCase()]) {
+        return `<span class="highlight">${americanToBritishTitles[word]}</span>`;
+      } else {
+        return word;
+      }
+    });
 
+    return translatedText.join(" ");
+  }
+
+  translateToAmerican(text) {
+    // const formattedText = text.toLowerCase();
+    const textArray = text.split(" ");
+
+    const translatedText = textArray.map((word) => {
+      if (britishOnly[word]) {
+        return `<span class="highlight">${
+          britishOnly[word.toLowerCase()]
+        }</span>`;
+      } else if (getKeyByValue(americanToBritishSpelling, word.toLowerCase())) {
+        return `<span class="highlight">${getKeyByValue(
+          americanToBritishSpelling,
+          word.toLowerCase()
+        )}</span>`;
+      } else if (getKeyByValue(americanToBritishTitles, word.toLowerCase())) {
+        return `<span class="highlight">${getKeyByValue(
+          americanToBritishTitles,
+          word.toLowerCase()
+        )}</span>`;
+      } else {
+        return word;
+      }
+    });
+
+    return translatedText.join(" ");
+  }
+
+  translate(text, locale) {
+    let translatedText = text;
+    if (locale === "american-to-british") {
+      translatedText = this.translateToBrittish(text);
+    } else if (locale === "british-to-american") {
+      translatedText = this.translateToAmerican(text);
+    }
+    return translatedText;
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,37 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+  app.route("/api/translate").post((req, res) => {
+    //locale will be 'american-to-british' or 'british-to-american'
+    // ex. { text: 'dgeg', locale: 'american-to-british' }
+    console.log(req.body);
+    const { text, locale } = req.body;
+
+    if (text === "") {
+      return res.json({ error: "No text to translate" });
+    }
+    if (!text || !locale) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+
+    if (locale !== "american-to-british" && locale !== "british-to-american") {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+
+    if (text === translator.translate(text, locale)) {
+      return res.json({
+        text: text,
+        translation: "Everything looks good to me!",
+      });
+    }
+
+    res.json({
+      text: text,
+      translation: translator.translate(text, locale),
     });
+  });
 };
